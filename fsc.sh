23f6881605081644e0a2fceb7b148ea8d9782d5a
CruncyChoco <45945815+unmeimusu@users.noreply.github.com>
2023-07-06 11:56:30 +0700

    Update fsc.sh

--- fsc.sh
+++ fsc.sh
@@ -1,1 +1,5 @@
-ffmpeg -f x11grab -s wxga -r 30 -i :0.0 -qscale 0 recording.mpg
+# list all available audio
+pactl list short sources
+# input one of list on the result above to script below for example mine is "ladspa_output.mbeq_1197.mbeq.monitor" without quotes
+# ffmpeg audio + video
+ffmpeg -f x11grab -video_size 1280x720 -framerate 30 -i :0.0 -f pulse -i ladspa_output.mbeq_1197.mbeq.monitor -preset fast -crf 22 -c:a aac -b:v 6M -pix_fmt yuv420p -b:a 128k ~/Videos/Recording-$(date +%F-%I-%M-%N).mp4
